ed4ac38f941c7a731c77fbf499f8733c900343e5
unknown <nidhitater23@gmail.com>
2020-10-10 21:12:35 +0530

    the dot issue fixed.

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ for (var i = 0; i < keys.length; i++) {
 
             decimalAdded = false;
         } else if (btnVal == '.') {
-            if (decimalAdded) {
+            if (!decimalAdded) {
                 input.innerHTML += btnVal;
                 decimalAdded = true;
             }
